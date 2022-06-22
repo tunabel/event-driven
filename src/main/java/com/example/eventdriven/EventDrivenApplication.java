45b4cf4268d03ad5d9d76b070df8677a22f64b28
Tuan Ngo <tuan.ngo1@ntq-solution.com.vn>
2022-06-22 14:59:37 +0700

    Initial commit

---
+++ src/main/java/com/example/eventdriven/EventDrivenApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventdriven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenApplication.class, args);
+	}
+
+}
